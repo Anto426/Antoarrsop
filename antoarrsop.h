00a41f55cc1d0a9573892ae83119f2588181a63d
Anto426 <amanocchio426@gmail.com>
2023-03-29 22:56:24 +0200

    update

--- antoarrsop.h
+++ antoarrsop.h
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <math.h>
 #include <stdio.h>
+#include <time.h>
 
 // arreymonodimensionali e bidimensionali
 /*
@@ -28,7 +29,7 @@
     |2 => arr2d |
     -------------
 
-           __mode
+          __mode
     --------------------
     |  ma = > automatic|
     |   mm => manual   |
@@ -36,10 +37,6 @@
 */
 
 // covertitore di putatori
-char *__putchars(void *p)
-{
-    return (char *)p;
-}
 int *__putint(void *p)
 {
     return (int *)p;
@@ -48,24 +45,45 @@ float *__putfloat(void *p)
 {
     return (float *)p;
 }
+char *__putchars(void *p)
+{
+    return (char *)p;
+}
 bool *__putbool(void *p)
 {
     return (bool *)p;
 }
 
 // funzioni random
-int __randomint()
+int __randomint(int plus)
 {
-    srand(time(NULL));
-    return 1 + rand();
+    int min = 0, max = 1000, intc;
+    srand(time(NULL)+ plus);
+    intc = rand() % (max - min + 1) + min;
+    return intc;
 }
-char __randomchar()
+
+float __randomfloat(int plus)
 {
-    __srand(time(NULL));
+    float min = 0.0, max = 1000.0, floatc;
+    srand(time(NULL)+ plus);
+    floatc = ((float)rand() / (float)RAND_MAX) * (max - min) + min;
+    return floatc;
+}
+char __randomchar(int plus)
+{
+    srand(time(NULL)+ plus);
     int num_casuale = rand() % 26;
     return 'a' + num_casuale;
 }
 
+bool __randombool(int plus)
+{
+    srand(time(NULL)+ plus);
+    int numero_casuale = rand();
+    bool booleano_casuale = (numero_casuale % 2 == 0) ? true : false;
+    return booleano_casuale;
+}
 // funzione stampa
 void __printpt(char __type, void *__po, char temp[], int _addbyte)
 {
@@ -101,21 +119,30 @@ void __inputpt(char __type, void *__po, char temp[], int _addbyte, char __mode)
     case 'd':
         _addbyte *= sizeof(int);
         if (__mode == 'a')
-            *(__putint(__pt + _addbyte)) = __randomint();
+            *(__putint(__pt + _addbyte)) = __randomint(_addbyte);
         else
             scanf(temp, (__putint(__pt + _addbyte)));
         break;
     case 'f':
         _addbyte *= sizeof(float);
-        scanf(temp, (__putfloat(__pt + _addbyte)));
+        if (__mode == 'a')
+            *(__putfloat(__pt + _addbyte)) = __randomfloat(_addbyte);
+        else
+            scanf(temp, (__putfloat(__pt + _addbyte)));
         break;
     case 'c':
         _addbyte *= sizeof(char);
-        scanf(temp, (__putchars(__pt + _addbyte)));
+        if (__mode == 'a')
+            *(__putchars(__pt + _addbyte)) = __randomchar(_addbyte);
+        else
+            scanf(temp, (__putchars(__pt + _addbyte)));
         break;
     case 'b':
         _addbyte *= sizeof(bool);
-        scanf(temp, (__putbool(__pt + _addbyte)));
+        if (__mode == 'a')
+            *(__putbool(__pt + _addbyte)) = __randombool(_addbyte);
+        else
+            scanf(temp, (__putbool(__pt + _addbyte)));
         break;
     default:
         exit(-1);
@@ -123,11 +150,11 @@ void __inputpt(char __type, void *__po, char temp[], int _addbyte, char __mode)
 }
 
 // vettori 2d
-void printarr(void *__arr, const char __type[2], int __nlenarr, int __dim)
+void printarr(void *__arr, int __nlenarr, const char __type[2])
 {
-    if (__type[0] != 't' || __arr == NULL || __dim > 1)
+    if (__type[0] != 't' || __arr == NULL || __nlenarr <= 1)
         exit(-1);
-    char temp[] = {'%', __type[1], ' '};
+    char temp[] = {'%', __type[1], '\t'};
     for (size_t i = 0; i < __nlenarr; i++)
     {
         __printpt(*(__type + 1), __arr, temp, i);
@@ -135,25 +162,25 @@ void printarr(void *__arr, const char __type[2], int __nlenarr, int __dim)
     return;
 }
 
-void fullarr(void *__arr, const char __type[2], int __nlenarr, int __dim, const char __mode[2])
+void fullarr(void *__arr, int __nlenarr, const char __type[2], const char __mode[2])
 {
-    if (__type[0] != 't' || __mode[0] != 'm' || __arr == NULL || __dim > 1)
+    if (__type[0] != 't' || __mode[0] != 'm' || __arr == NULL || __nlenarr <= 1)
         exit(-1);
     char temp[] = {'%', __type[1]};
 
     for (size_t i = 0; i < __nlenarr; i++)
     {
-        __inputpt(*(__type + 1), __arr, temp, i);
+        __inputpt(*(__type + 1), __arr, temp, i, *(__mode + 1));
     }
     return;
 }
 
 // vettori 3d
-void printmaatt(int __nlenarrR, int __nlenarrC, void *__matt, const char __type[2])
+void printmaatt(void *__matt, int __nlenarrR, int __nlenarrC, const char __type[2])
 {
     if (__type[0] != 't' || __matt == NULL)
         exit(-1);
-    char temp[] = {'%', __type[1]};
+    char temp[] = {'%', __type[1], '\t'};
     for (size_t i = 0; i < __nlenarrR * __nlenarrC; i++)
     {
         if (i != 0 && i % __nlenarrC == 0)
@@ -164,19 +191,15 @@ void printmaatt(int __nlenarrR, int __nlenarrC, void *__matt, const char __type[
     }
 }
 
-void fullmaatt(int __nlenarrR, int __nlenarrC, void *__matt, const char __type[2], const char __mode[])
+void fullmaatt(void *__matt, int __nlenarrR, int __nlenarrC, const char __type[2], const char __mode[2])
 {
-    if (__type[0] != 't' || __matt == NULL)
+    if (__type[0] != 't' || __mode[0] != 'm' || __matt == NULL)
         exit(-1);
-    char temp[] = {'%', __type[1], ' '};
+    char temp[] = {'%', __type[1]};
 
     for (size_t i = 0; i < __nlenarrR * __nlenarrC; i++)
     {
-        if (i != 0 && i % __nlenarrC == 0)
-        {
-            printf("\n");
-        }
-        __printpt(*(__type + 1), __matt, temp, i);
+        __inputpt(*(__type + 1), __matt, temp, i, *(__mode + 1));
     }
 }
 #endif
--- prova.c
+++ prova.c
@@ -1,26 +1,9 @@
 #include "antoarrsop.h"
 #include <stdio.h>
-
-void fullarray(int n, char (*__matt)[n], int *na)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int z = 0; z < n; z++)
-        {
-            printf("Inserisci il %d° elemento nella %d° riga del %d° array: ", z + 1, i + 1, *na);
-
-            // Pulisci il buffer di input
-            while (getchar() != '\n');
-
-            scanf("%c", &__matt[i][z]);
-        }
-    }
-    (*na)++;
-}
-
 int main()
 {
-    bool A[3][3] = {{1,0,1}, {1,0,1}, {0,1,0}};
+    int A[3];
+    fullarr(A, 3, "td", "ma");
     printf("Ecco l'arrey:\n");
-    printmaatt(3, 3, A, "tb");
+    printarr(A, 3, "td");
 }
